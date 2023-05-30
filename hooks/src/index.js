c0aa17cdc4475974ce80f318a68c658dee634a2c
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2023-05-30 20:40:13 -0300

    Alteração no projeto e criação de novas classes

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,10 +1,10 @@
-import ReactDom from 'react-dom'
-import React from 'react'
 import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
 
 import App from './views/App'
 
-ReactDom.render(
+ReactDOM.render(
     <App />,
     document.getElementById('root')
 )
