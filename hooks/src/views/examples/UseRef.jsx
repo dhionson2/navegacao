fbd703098578f668c388e643f13d66c00d09a73a
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2023-06-01 20:04:56 -0300

    Dois Exemplos Simples de UseRef

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -1,13 +1,60 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+
+const marge = function(s1 , s2) {
+    return [...s1].map(function(e,i){
+        return `${e}${s2[i] || "" }`;
+    }).join("")
+        
+}
 
 const UseRef = (props) => {
+    const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
+    const count = useRef(0)
+
+    const myImput1 = useRef(null)
+    const myImput2 = useRef(null)
+
+    console.log(myImput1.current);
+    //sempre que ouver uma alteração no value1 ele vai chamar o useEfect e somar mas 1
+    //onde tem ,[value1]), eu posso adicionar ,[value1,value2]), e ele vai ser chamado quando alguma das duas dependencias for modificadas
+    useEffect(function(){
+        count.current = count.current + 1
+        myImput2.current.focus()
+    },[value1])
+
+    useEffect(function(){
+        count.current = count.current + 1
+        myImput1.current.focus()
+    },[value2])
+    
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
-            />
+                subtitle="Retorna um objeto mutável com a propriedade .current!"/>
+                <SectionTitle title="Exemplo #01"/>
+                <div className='center'>
+                    <div>
+                        <span className="text">Valor </span>
+                        <span className="text">{marge(value1, value2)} [</span>
+                        <span className="text red">{count.current}</span>
+                        <span className="text">]</span>
+                    </div>
+                    <input type="text" className="input" ref={myImput1} value={value1} onChange={e => setValue1(e.target.value)}/>
+
+                </div>
+                <SectionTitle title="Exemplo #02"/>
+                <div className="center">
+                    <input type="text" className="input" ref={myImput2} value={value2} onChange= {e => setValue2(e.target.value)}/>
+
+                </div>
         </div>
     )
 }
